5ea071e003d83b0b253591ac6846799bc3168674
zerogyro <jinyu.gu1997uiuc@gmail.com>
2023-05-22 21:34:49 +0900

    format

--- kernel_utils.py
+++ kernel_utils.py
@@ -5,97 +5,116 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
 
-
-
 def get_transition_matrix(calib_file=None):
-    '''Get transition
+    """Get transition
     input: calib_file
     output: transition matrix mapping from point cloud to image domain
-    '''
-    calib_file = 'calib/calib.txt'
-    with open(calib_file, 'r') as f:
+    """
+    calib_file = "calib/calib.txt"
+    with open(calib_file, "r") as f:
         calib = f.readlines()
 
-
     # P2 (3 x 4) for left eye
-    P2 = np.matrix([float(x) for x in calib[2].strip('\n').split(' ')[1:]]).reshape(3,4)
-    R0_rect = np.matrix([float(x) for x in calib[4].strip('\n').split(' ')[1:]]).reshape(3,3)
+    P2 = np.matrix([float(x) for x in calib[2].strip("\n").split(" ")[1:]]).reshape(
+        3, 4
+    )
+    R0_rect = np.matrix(
+        [float(x) for x in calib[4].strip("\n").split(" ")[1:]]
+    ).reshape(3, 3)
     # Add a 1 in bottom-right, reshape to 4 x 4
-    R0_rect = np.insert(R0_rect,3,values=[0,0,0],axis=0)
-    R0_rect = np.insert(R0_rect,3,values=[0,0,0,1],axis=1)
-    Tr_velo_to_cam = np.matrix([float(x) for x in calib[5].strip('\n').split(' ')[1:]]).reshape(3,4)
-    Tr_velo_to_cam = np.insert(Tr_velo_to_cam,3,values=[0,0,0,1],axis=0)
-    
-    
+    R0_rect = np.insert(R0_rect, 3, values=[0, 0, 0], axis=0)
+    R0_rect = np.insert(R0_rect, 3, values=[0, 0, 0, 1], axis=1)
+    Tr_velo_to_cam = np.matrix(
+        [float(x) for x in calib[5].strip("\n").split(" ")[1:]]
+    ).reshape(3, 4)
+    Tr_velo_to_cam = np.insert(Tr_velo_to_cam, 3, values=[0, 0, 0, 1], axis=0)
+
     return P2, R0_rect, Tr_velo_to_cam
 
 
 def parse_args_and_config():
-
-    parser = argparse.ArgumentParser(description=globals()['__doc__'])
-    # file_config 
-    parser.add_argument('--file_name', type=str, required=False, default = 'aaa',help='filename for path to processing file')
-    parser.add_argument('--test_folder', type = str, default= 'test_data', help = 'directory for occlusion area conversion')
-    parser.add_argument('--bin_path', type = str, default= '0000000000.bin', help = 'path for point cloud file')
-    parser.add_argument('--img_path', type = str, default= '0000000000.png', help = 'path for image file')
+    parser = argparse.ArgumentParser(description=globals()["__doc__"])
+    # file_config
+    parser.add_argument(
+        "--file_name",
+        type=str,
+        required=False,
+        default="aaa",
+        help="filename for path to processing file",
+    )
+    parser.add_argument(
+        "--test_folder",
+        type=str,
+        default="test_data",
+        help="directory for occlusion area conversion",
+    )
+    parser.add_argument(
+        "--bin_path",
+        type=str,
+        default="0000000000.bin",
+        help="path for point cloud file",
+    )
+    parser.add_argument(
+        "--img_path", type=str, default="0000000000.png", help="path for image file"
+    )
     args = parser.parse_args()
 
     return args
+
+
 # initialize a kernel method class
 
+
 class Kernel_tool(object):
-    def __init__(self,args):
-        #TODO: get_trainsition_matrix from args
+    def __init__(self, args):
+        # TODO: get_trainsition_matrix from args
 
-        self.bin_path = os.path.join(args.test_folder, args.bin_path) 
-        self.img_path = os.path.join(args.test_folder, args.img_path) 
-        
+        self.bin_path = os.path.join(args.test_folder, args.bin_path)
+        self.img_path = os.path.join(args.test_folder, args.img_path)
+
         self.P2, self.R0_rect, self.Tr_velo_to_cam = get_transition_matrix()
-        
 
-    
-    #TODO: get_mapped_points from args
+    # TODO: get_mapped_points from args
     def get_mapped_points(self):
-        '''
+        """
         this function is to pre-processing the data from mapping point cloud points to image domain
         input: point cloud data path, img data path
         output: mapped points and corresponding image pixels
-        '''
+        """
         binary_path, img_path = self.bin_path, self.img_path
         png = mpimg.imread(img_path)
-        IMG_H,IMG_W,_ = png.shape
+        IMG_H, IMG_W, _ = png.shape
         # read raw data from binary
-        scan = np.fromfile(binary_path, dtype=np.float32).reshape((-1,4))
-        points = scan[:, 0:3] # lidar xyz (front, left, up)
-        #print(points[0])
-        velo = np.insert(points,3,1,axis=1).T
-        velo = np.delete(velo,np.where(velo[0,:]<0),axis=1)
-        cam = self.P2 *  self.R0_rect * self.Tr_velo_to_cam * velo
+        scan = np.fromfile(binary_path, dtype=np.float32).reshape((-1, 4))
+        points = scan[:, 0:3]  # lidar xyz (front, left, up)
+        # print(points[0])
+        velo = np.insert(points, 3, 1, axis=1).T
+        velo = np.delete(velo, np.where(velo[0, :] < 0), axis=1)
+        cam = self.P2 * self.R0_rect * self.Tr_velo_to_cam * velo
 
         # 1 to 1 mapping for new_velo and cam
-        cam_index = np.where(cam[2,:]>=0)[1]
+        cam_index = np.where(cam[2, :] >= 0)[1]
         new_velo = np.take(velo, cam_index, axis=1)
-        cam = np.delete(cam,np.where(cam[2,:]<0)[1],axis=1)
+        cam = np.delete(cam, np.where(cam[2, :] < 0)[1], axis=1)
         # get u,v,z
-        cam[:2] /= cam[2,:]
-        u,v,z = cam
-        u_out = np.logical_or(u<5, u>IMG_W-5)
-        v_out = np.logical_or(v<5, v>IMG_H-5)
+        cam[:2] /= cam[2, :]
+        u, v, z = cam
+        u_out = np.logical_or(u < 5, u > IMG_W - 5)
+        v_out = np.logical_or(v < 5, v > IMG_H - 5)
         outlier = np.logical_or(u_out, v_out)
-        
-        new_velo = np.delete(new_velo,np.where(outlier),axis=1)
-        new_cam = np.delete(cam,np.where(outlier),axis=1)
-        new_cam = np.asarray(new_cam)
-        return new_velo,new_cam,png
 
-    
+        new_velo = np.delete(new_velo, np.where(outlier), axis=1)
+        new_cam = np.delete(cam, np.where(outlier), axis=1)
+        new_cam = np.asarray(new_cam)
+        return new_velo, new_cam, png
+
     def debug(self):
-        print(self.test_dir)
+        # print(self.test_dir)
+        print("debug mode")
+
 
 if __name__ == "__main__":
-
-    
     args = parse_args_and_config()
-    k_tool = Kernel_tool(args= args)
-    _,_,_ = k_tool.get_mapped_points()
+    k_tool = Kernel_tool(args=args)
+    _, _, _ = k_tool.get_mapped_points()
     k_tool.debug()
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import argparse
 
 
-
-
-
-
 def parse_args_and_config():
-
-    parser = argparse.ArgumentParser(description=globals()['__doc__'])
-    # file_config 
-    parser.add_argument('--file_name', type=str, required=False, default = 'aaa',help='filename for path to processing file')
+    parser = argparse.ArgumentParser(description=globals()["__doc__"])
+    # file_config
+    parser.add_argument(
+        "--file_name",
+        type=str,
+        required=False,
+        default="aaa",
+        help="filename for path to processing file",
+    )
 
     args = parser.parse_args()
 
